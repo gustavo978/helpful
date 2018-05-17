90fc5f7a0a1e44a512e738d41b41169c534f2979
Gustavo Araújo <inf.araujo@gmail.com>
2018-05-18 03:38:15 +0800

    Remove mininet and ns-3.22 dir

--- mininet/compilep4.sh
+++
@@ -1,1 +0,0 @@
-p4c-bmv2 --json ../p4src/p4BaseStation.json ../p4src/p4BaseStation.p4
--- ns-3.22/build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/subdir/ns3.22-subdir-debug', 'build/scratch/ns3.22-scratch-simulator-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
